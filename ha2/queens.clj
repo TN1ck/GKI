a01a3265a8e0ed61c1f6f22f92fb4379a3feaeff
TN1ck <tomwanick@gmail.com>
2013-11-10 15:40:15 +0100

    added stack-based variant

--- ha2/queens.clj
+++ ha2/queens.clj
@@ -1,7 +1,7 @@
 (ns queens)
 
-(defn solve [n]
-  "solves the queen-problem for a given n"
+(defn solve-with-stepper [n]
+  "use a stepper instead of a stack, performs better"
   (let [domains-initial (vec (for [i (range n)] (vec (range n))))
         constrains [(fn [state] (= (count (distinct state)) (count state)))
                     (fn [state] (let [j (dec (count state))]
@@ -23,4 +23,23 @@
         state
         (recur (progress-state (cons 0 state) domains-initial) domains)))))
 
-(solve 16)
+(solve-with-stepper 4)
+
+(defn solve-stack [n]
+  "exacly the algorithm mentioned in the exercise"
+  (let [domains (vec (for [i (range n)] (vec (range n))))
+        stack [[]]
+        constrains [(fn [state] (= (count (distinct state)) (count state)))
+                    (fn [state] (let [j (dec (count state))]
+                                  (every? true? (for [i (range 0 j)]
+                                                  (not= (Math/abs (- i j))
+                                                        (Math/abs (- ((domains i) (nth state (- j i)))
+                                                                     ((domains j) (first state)))))))))]]
+    (loop [[x & xs] stack]
+      (if (or (nil? x) (= n (count x)))
+        x
+        (recur (concat (for [i (domains (count x))
+                             :when (every? true? (map #(% (cons i x)) constrains))]
+                         (cons i x)) xs))))))
+
+(solve-stack 10)
