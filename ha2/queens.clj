2d40f1ee582ac52b7e783071230961e43f013b88
TN1ck <tomwanick@gmail.com>
2013-11-10 13:57:44 +0100

    cleaned up

--- ha2/queens.clj
+++ ha2/queens.clj
@@ -1,25 +1,5 @@
 (ns queens)
 
-;(defn progress-state [state queens]
-;  "a state looks like this: [0 1 2 3 4 5] on a 6-queens problem, get the next step"
-;  (loop [i 0 [x & xs] (reverse state) s [] r true]
-;    (if (and r (not (nil? x)))
-;      (let [[n r-n] (if (= x (last (queens i)))
-;                        [(first (queens i)) true]
-;                        [(inc x) false])]
-;        (recur (inc i) xs (conj s n) r-n))
-;      (vec (reverse (concat s  (if (nil? x) xs (cons x xs))))))))
-;
-; (defn progress-state [state queens]
-;  "a state looks like this: [0 1 2 3 4 5] on a 6-queens problem, get the next step"
-;  (loop [i 0 [x & xs] (reverse state) s [] r true]
-;    (if (and r (not (nil? x)))
-;      (let [[n r-n] (if (= x (last (queens i)))
-;                        [(first (queens i)) true]
-;                        [(inc x) false])]
- ;       (recur (inc i) xs (conj s n) r-n))
- ;     (vec (reverse (concat s  (if (nil? x) xs (cons x xs))))))))
-
 (defn solve [n]
   "solves the queen-problem for a given n"
   (let [domain-initial (vec (for [i (range n)] (vec (range n))))
@@ -43,4 +23,4 @@
         state
         (recur (progress-state (cons 0 state) domain-initial) domain)))))
 
-(solve 14)
+(solve 8)
